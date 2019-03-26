1505241250080ffd20f88f6c6d032ffdc79a1264
Peter Pham <phamhere@gmail.com>
2019-03-26 15:52:27 -0600

    added delete function placeholder

--- script.js
+++ script.js
@@ -3,5 +3,6 @@ const todoList = document.querySelector('#todo-list');
 const todos = [{ id: 0, title: 'test', completed: false, nested: [] }];
 
 const App = {
-  create: function() {}
+  create: function() {},
+  delete: function() {}
 };
